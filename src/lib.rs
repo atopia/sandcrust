c38b9668b42c04289e278b8635e87215b5a9e845
Benjamin Lamowski <benjamin@lamowski.net>
2016-11-15 17:01:24 +0100

    use sandheap as a sandbox prototype

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 extern crate nix;
+extern crate sandheap;
 
 use nix::unistd::{fork, ForkResult};
+use sandheap::sandheap_setup;
 
 #[cfg(test)]
 mod tests {
@@ -14,7 +16,10 @@ pub fn sandbox_me() {
         Ok(ForkResult::Parent { child, .. }) => {
             println!("parent: new child has pid: {}", child);
         }
-        Ok(ForkResult::Child) => println!("child: miau"),
-        Err(e) => println!("Fork failed with error {}", e),
+        Ok(ForkResult::Child) => {
+            println!("child: miau");
+            sandheap_setup();
+        }
+        Err(e) => println!("Fork failed with error {}", e)
     }
 }
