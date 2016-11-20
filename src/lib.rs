4b2e0fc187a73418c68a173f8004e98fe483f030
Benjamin Lamowski <benjamin@lamowski.net>
2016-11-20 16:55:08 +0100

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ extern crate sandheap;
 
 use libc::{readlink, c_char};
 use std::ffi::CString;
-use errno::{errno};
+use errno::errno;
 
 use nix::unistd::{fork, ForkResult, getpid};
 use sandheap as sandbox;
@@ -14,24 +14,22 @@ use sandheap as sandbox;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
 
 fn get_mnt_ns() {
     let pid = getpid();
     // FIXME some nicer way to build a path?
-    let pathstr =  "/proc/".to_string() + &pid.to_string() + "/ns/mnt";
+    let pathstr = "/proc/".to_string() + &pid.to_string() + "/ns/mnt";
     let path = CString::new(pathstr).unwrap();
 
     // jeez this is ugly as fuck
-    let mut x : Vec<c_char> = vec![0; 256];
+    let mut x: Vec<c_char> = vec![0; 256];
     let slice = x.as_mut_slice();
     let bufptr = slice.as_mut_ptr();
 
     unsafe {
-        if readlink(path.as_ptr(), bufptr, 255) > 0
-        {
+        if readlink(path.as_ptr(), bufptr, 255) > 0 {
             let contents = CString::from_raw(bufptr).into_string().unwrap();
             println!("mnt ns: {}", contents);
         } else {
@@ -52,6 +50,6 @@ pub fn sandbox_me() {
             println!("CHILD:");
             get_mnt_ns();
         }
-        Err(e) => println!("Fork failed with error {}", e)
+        Err(e) => println!("Fork failed with error {}", e),
     }
 }
