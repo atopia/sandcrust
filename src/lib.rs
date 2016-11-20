1eb13af0955f7aa274f8feb10849fc961cdeb7f2
Benjamin Lamowski <benjamin@lamowski.net>
2016-11-20 17:05:24 +0100

    pass a function w/o arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
 extern crate nix;
-extern crate libc;
-extern crate errno;
 
 extern crate sandheap;
 
-use libc::{readlink, c_char};
-use std::ffi::CString;
-use errno::errno;
-
-use nix::unistd::{fork, ForkResult, getpid};
+use nix::unistd::{fork, ForkResult};
 use sandheap as sandbox;
 
 #[cfg(test)]
@@ -17,38 +11,17 @@ mod tests {
     fn it_works() {}
 }
 
-fn get_mnt_ns() {
-    let pid = getpid();
-    // FIXME some nicer way to build a path?
-    let pathstr = "/proc/".to_string() + &pid.to_string() + "/ns/mnt";
-    let path = CString::new(pathstr).unwrap();
 
-    // jeez this is ugly as fuck
-    let mut x: Vec<c_char> = vec![0; 256];
-    let slice = x.as_mut_slice();
-    let bufptr = slice.as_mut_ptr();
-
-    unsafe {
-        if readlink(path.as_ptr(), bufptr, 255) > 0 {
-            let contents = CString::from_raw(bufptr).into_string().unwrap();
-            println!("mnt ns: {}", contents);
-        } else {
-            let e = errno();
-            println!("read failed: {}", e);
-        }
-    }
-}
-
-pub fn sandbox_me() {
+pub fn sandbox_me(func: fn()) {
     match fork() {
         Ok(ForkResult::Parent { child, .. }) => {
             println!("PARENT (child pid {}):", child);
-            get_mnt_ns();
+            func();
         }
         Ok(ForkResult::Child) => {
             sandbox::setup();
             println!("CHILD:");
-            get_mnt_ns();
+            func();
         }
         Err(e) => println!("Fork failed with error {}", e),
     }
--- src/sandstorm.rs
+++ src/sandstorm.rs
@@ -1,7 +1,40 @@
 extern crate sandcrust;
 
+extern crate nix;
+extern crate libc;
+extern crate errno;
+
+use nix::unistd::getpid;
+use libc::{readlink, c_char};
+use std::ffi::CString;
+use errno::errno;
+
 use sandcrust::sandbox_me;
 
+
+pub fn get_mnt_ns() {
+    let pid = getpid();
+    // FIXME some nicer way to build a path?
+    let pathstr = "/proc/".to_string() + &pid.to_string() + "/ns/mnt";
+    let path = CString::new(pathstr).unwrap();
+
+    // jeez this is ugly as fuck
+    let mut x: Vec<c_char> = vec![0; 256];
+    let slice = x.as_mut_slice();
+    let bufptr = slice.as_mut_ptr();
+
+    unsafe {
+        if readlink(path.as_ptr(), bufptr, 255) > 0 {
+            let contents = CString::from_raw(bufptr).into_string().unwrap();
+            println!("mnt ns: {}", contents);
+        } else {
+            let e = errno();
+            println!("read failed: {}", e);
+        }
+    }
+}
+
+
 pub fn main() {
-    sandbox_me();
+    sandbox_me(get_mnt_ns);
 }
