2bad4fb401f2310d0654f003a21b55f98dea3d3b
Benjamin Lamowski <benjamin@lamowski.net>
2016-12-07 16:45:32 +0100

    refactored to Sandcrust struct

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,15 @@ extern crate nix;
 extern crate sandheap;
 extern crate memmap;
 
+// FIXME still too many exported uses
 pub use nix::unistd::{fork, ForkResult};
 pub use nix::libc::pid_t;
 pub use std::process::exit;
+// FIXME demo code
+pub use std::mem::size_of;
 
-use std::fs::{OpenOptions,remove_file};
-use std::mem::size_of;
-use nix::sys::wait::{waitpid};
+use std::fs::{OpenOptions, remove_file};
+use nix::sys::wait::waitpid;
 use memmap::{Mmap, Protection};
 
 use sandheap as sandbox;
@@ -21,25 +23,24 @@ mod tests {
 }
 
 
-struct Shm{
-    file_mmap : Mmap,
+struct Shm {
+    file_mmap: Mmap,
 }
 
 
 // FIXME nicer error handling and stuff
-impl Shm{
-    fn new(size : u64) -> Shm{
+impl Shm {
+    fn new(size: u64) -> Shm {
         let path: &'static str = "/dev/shm/sandcrust_shm";
         let f = OpenOptions::new()
             .read(true)
             .write(true)
             .create(true)
-            .open(path).unwrap();
+            .open(path)
+            .unwrap();
         f.set_len(size).unwrap();
         remove_file(path).unwrap();
-        Shm {
-            file_mmap : Mmap::open(&f, Protection::ReadWrite).unwrap(),
-        }
+        Shm { file_mmap: Mmap::open(&f, Protection::ReadWrite).unwrap() }
     }
 
     fn as_ptr(&mut self) -> *mut u8 {
@@ -47,53 +48,53 @@ impl Shm{
     }
 }
 
-// needed so that the macro actually finds its shit
+
+// needed as a wrapper for all the imported uses
 #[doc(hidden)]
-pub fn sandbox_setup_shm() {
-        let size = size_of::<u8>() as u64;
-        let mut shm = Shm::new(size);
-        let memptr = shm.as_ptr();
-        let memref : &mut u8 = unsafe { &mut *memptr };
-        println!("memref1 is {}", memref);
-        //let middle = unsafe { memptr.offset(2048) };
-        *memref = 23;
-        println!("memref2 is {}", memref);
+pub struct Sandcrust {
+    shm: Shm,
 }
 
-#[doc(hidden)]
-pub fn sandbox_do_parenting(child : pid_t) {
-        // FIXME this should be w/ locking
-        //*memref = 42;
+impl Sandcrust {
+    pub fn new(size: usize) -> Sandcrust {
+        let size = size as u64;
+        Sandcrust { shm: Shm::new(size) }
+    }
+
+    pub fn setup_child(&mut self) {
+        sandbox::setup();
+
+        // FIXME demo code
+        let newptr = self.shm.as_ptr();
+        let newval: &mut u8 = unsafe { &mut *newptr };
+        println!("CHILD: newval is {}", newval);
+        *newval = 161;
+        println!("CHILD: set newval to {}", newval);
+    }
+
+    pub fn join_child(&mut self, child: pid_t) {
         match waitpid(child, None) {
             Ok(_) => println!("sandcrust: waitpid() successful"),
             Err(e) => println!("sandcrust waitpid() failed with error {}", e),
         }
-        //println!("PARENT: memref is now {}", memref);
-}
 
-
-#[doc(hidden)]
-pub fn sandbox_setup_child(){
-        sandbox::setup();
-        // don't do any of that shit because we don't have the global shm -> create options once
-        // that's ready
-//        let newptr = shm.as_ptr();
-//        let newval : &mut u8 = unsafe { &mut *newptr};
-//        println!("CHILD: newval is {}", newval);
-//        *newval = 161;
-//        println!("CHILD: set newval to {}", newval);
+        // FIXME demo code
+        let memptr = self.shm.as_ptr();
+        let memref: &mut u8 = unsafe { &mut *memptr };
+        println!("PARENT: memref is now {}", memref);
+    }
 }
 
 
 #[macro_export]
 macro_rules! sandbox_me {
     ($f:ident($($x:expr ),*)) => {{
-        sandbox_setup_shm();
+        let mut sandcrust = Sandcrust::new(size_of::<u8>());
 
         match fork() {
-            Ok(ForkResult::Parent { child, .. }) => sandbox_do_parenting(child),
+            Ok(ForkResult::Parent { child, .. }) => sandcrust.join_child(child),
             Ok(ForkResult::Child) => {
-                sandbox_setup_child();
+                sandcrust.setup_child();
                 $f($($x),*);
                 exit(0);
             }
