c1896004223b57b04a369b6c9e00b1e13d7c224a
Benjamin Lamowski <benjamin@lamowski.net>
2017-01-04 13:41:38 +0100

    more tests for multiple argument forms

--- tests/val_restore.rs
+++ tests/val_restore.rs
@@ -28,4 +28,55 @@ mod val_restore {
         sandbox_me!(second_to_first(&mut a, b));
         assert!(a == 42);
     }
+
+    fn first_to_second(a : i32, b : &mut i32) {
+        *b = a;
+    }
+
+    #[test]
+    fn first_to_second_test() {
+        let a = 23;
+        let mut b = 42;
+        sandbox_me!(first_to_second(a, &mut b));
+        assert!(b == 23);
+    }
+
+    fn mult_args_ref_direct_1(a: &i32, b: &mut i32, c: i32) {
+        *b = a + c;
+    }
+
+    #[test]
+    fn mult_args_ref_direct_1_test() {
+        let a = 1;
+        let mut b = 2;
+        let c = 3;
+        sandbox_me!(mult_args_ref_direct_1(&a, &mut b, c));
+        assert!(b == 4);
+    }
+
+    fn mult_args_ref_direct_2(a: i32, b: &i32, c: &mut i32) {
+        *c = a + b;
+    }
+
+    #[test]
+    fn mult_args_ref_direct_2_test() {
+        let a = 1;
+        let b = 2;
+        let mut c = 7;
+        sandbox_me!(mult_args_ref_direct_2(a, &b, &mut c));
+        assert!(c == 3);
+    }
+
+    fn mult_args_ref_direct_3(a: &mut i32, b: &i32, c: i32) {
+        *a = b + c;
+    }
+
+    #[test]
+    fn mult_args_ref_direct_3_test() {
+        let mut a = 1;
+        let b = 2;
+        let c = 3;
+        sandbox_me!(mult_args_ref_direct_3(&mut a, &b, c));
+        assert!(a == 5);
+    }
 }
