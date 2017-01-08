eb8c4a86440d0752068b241d68d71c9aa99d002e
Benjamin Lamowski <benjamin@lamowski.net>
2017-01-08 13:11:16 +0100

    first basic FFI test

---
+++ tests/val_restore_ffi.rs
@@ -0,0 +1,23 @@
+extern crate sandcrust;
+extern crate libc;
+
+#[cfg(test)]
+mod val_restore_ffi {
+    use libc::{c_char, c_int};
+    use std::ffi::CString;
+
+    use sandcrust::*;
+
+    extern {
+        fn puts(s: *const c_char) -> c_int;
+    }
+
+    #[test]
+    fn puts_test() {
+        let greeting = CString::new("Hello libc").unwrap();
+        unsafe {
+            let gp = greeting.as_ptr();
+            puts(gp);
+        }
+    }
+}
