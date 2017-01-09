06b30e769fe176a9a39e2065085d4ac8b8a57fd6
Benjamin Lamowski <benjamin@lamowski.net>
2017-01-09 15:36:19 +0100

    working FFI snprintf as a base, w/o sandcrust

--- src/sandstorm.rs
+++ src/sandstorm.rs
@@ -1,43 +1,48 @@
 extern crate sandcrust;
-
-extern crate nix;
 extern crate libc;
-extern crate errno;
 
-use nix::unistd::getpid;
-use libc::{readlink, c_char};
-use std::ffi::CString;
-use errno::errno;
 
 use sandcrust::*;
+use std::ffi::CString;
+use libc::*;
 
-
-fn get_mnt_ns() {
-    let pid = getpid();
-    // FIXME some nicer way to build a path?
-    let pathstr = "/proc/".to_string() + &pid.to_string() + "/ns/mnt";
-    let path = CString::new(pathstr).unwrap();
-
-    // jeez this is ugly as fuck
-    let mut x: Vec<c_char> = vec![0; 256];
-    let slice = x.as_mut_slice();
-    let bufptr = slice.as_mut_ptr();
-
-    unsafe {
-        if readlink(path.as_ptr(), bufptr, 255) > 0 {
-            let contents = CString::from_raw(bufptr).into_string().unwrap();
-            println!("mnt ns: {}", contents);
-        } else {
-            let e = errno();
-            println!("read failed: {}", e);
-        }
-    }
+#[link="c"]
+extern {
+	fn snprintf(str: *mut c_char, size: size_t, format: *const c_char, ...) -> c_int;
 }
 
+/*
+fn sprintf1(format: &str, value: ???) -> String {
+	const BUF_SIZE: usize = 256;
+	unsafe {
+			let format = CString::new(format).unwrap().into_raw();
+			let buffer = CString::new(&Vec::with_capacity(BUF_SIZE)).unwrap().into_raw();
 
-pub fn main() {
-    println!("PARENT: now sandboxing child");
-    sandbox_me!(get_mnt_ns());
-    println!("PARENT:");
-    get_mnt_ns();
+			snprintf(format, BUF_SIZE as size_t, buffer, value);
+
+			let result = CString::from_raw(buffer);
+			from_utf8(result).unwrap().to_string()
+		}
+}
+*/
+
+
+
+fn main() {
+	let size: size_t = 256;
+	let formatstr = CString::new("I am %s, of %d years\n").unwrap();
+	let mut vec = Vec::with_capacity(size);
+
+	let namestr = CString::new("Ben").unwrap();
+	let age: c_uint = 31;
+	unsafe {
+		let fmt = formatstr.as_ptr();
+		let name = namestr.as_ptr();
+		let buf = vec.as_mut_ptr() as *mut i8;
+		let len = snprintf(buf, size, fmt, name, age);
+		vec.set_len(len as usize);
+//		let result = CString::from_raw(buf);
+	}
+	let stringy = String::from_utf8(vec).unwrap();
+	println!("string is {}", stringy);
 }
