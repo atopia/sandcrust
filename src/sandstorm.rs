556123a314b1b64ab6919d0441bec7517ac7c6c8
Benjamin Lamowski <benjamin@lamowski.net>
2017-01-12 02:39:46 +0100

    test for multiple args

--- src/sandstorm.rs
+++ src/sandstorm.rs
@@ -11,12 +11,13 @@ extern {
 	fn snprintf(str: *mut c_char, size: size_t, format: *const c_char, ...) -> c_int;
 }
 
-fn snprintf_wrapper(vec: &mut Vec<u8>, size: size_t, format: *const c_char, name: *const c_char, age: c_uint) {
+fn snprintf_wrapper(vec: &mut Vec<u8>, size: size_t, format: *const c_char, name: *const c_char, age: c_uint, len: &mut c_int) {
     println!("wrapped: PID: {}", nix::unistd::getpid());
 	unsafe {
 		let buf = vec.as_mut_ptr() as *mut i8;
-		let len = snprintf(buf, size, format, name, age);
-		vec.set_len(len as usize);
+		let lenny = snprintf(buf, size, format, name, age);
+		vec.set_len(lenny as usize);
+        *len = lenny;
 	}
 }
 
@@ -30,7 +31,8 @@ fn main() {
 	let name = namestr.as_ptr();
 	let age: c_uint = 31;
     println!("orig: PID: {}", nix::unistd::getpid());
-    sandbox_me!(snprintf_wrapper(&mut vec, size, fmt, name, age));
+    let mut len: c_int = 0;
+    sandbox_me!(snprintf_wrapper(&mut vec, size, fmt, name, age, &mut len));
 	let stringy = String::from_utf8(vec).unwrap();
-	println!("string is {}", stringy);
+	println!("string is {} with new len {}", stringy, len);
 }
