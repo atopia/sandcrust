8d4624dc215cc7bc10c6fe059ba2526ae757b501
Benjamin Lamowski <benjamin@lamowski.net>
2017-01-12 02:11:20 +0100

    sandstorm: wrapper for snprintf

--- src/sandstorm.rs
+++ src/sandstorm.rs
@@ -10,20 +10,24 @@ extern {
 	fn snprintf(str: *mut c_char, size: size_t, format: *const c_char, ...) -> c_int;
 }
 
+fn snprintf_wrapper(vec: &mut Vec<u8>, size: size_t, format: *const c_char, name: *const c_char, age: c_uint) {
+	unsafe {
+		let buf = vec.as_mut_ptr() as *mut i8;
+		let len = snprintf(buf, size, format, name, age);
+		vec.set_len(len as usize);
+	}
+}
+
 fn main() {
 	let size: size_t = 256;
 	let formatstr = CString::new("I am %s, of %d years\n").unwrap();
 	let mut vec = Vec::with_capacity(size);
 
 	let namestr = CString::new("Ben").unwrap();
+	let fmt = formatstr.as_ptr();
+	let name = namestr.as_ptr();
 	let age: c_uint = 31;
-	unsafe {
-		let fmt = formatstr.as_ptr();
-		let name = namestr.as_ptr();
-		let buf = vec.as_mut_ptr() as *mut i8;
-		let len = snprintf(buf, size, fmt, name, age);
-		vec.set_len(len as usize);
-	}
+    snprintf_wrapper(&mut vec, size, fmt, name, age);
 	let stringy = String::from_utf8(vec).unwrap();
 	println!("string is {}", stringy);
 }
