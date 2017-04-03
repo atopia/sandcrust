cce20ae6df259a85a23e8d9e616cd2764cc86bf6
Benjamin Lamowski <benjamin@lamowski.net>
2017-04-03 17:33:38 +0200

    fix typo in warning

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! functions in a sandboxed process.
 //!
 //! This is a highly experimental prototype, **do not use in production!**
-#![warn(missing_docs, missing_debug_implementations, trivial_casts, trivial_numeric_casts, unstable_features, unused_extern_crates, unused_results, unused_import_braces, unused_qualifications, variant-size-differences)]
+#![warn(missing_docs, missing_debug_implementations, trivial_casts, trivial_numeric_casts, unstable_features, unused_extern_crates, unused_results, unused_import_braces, unused_qualifications, variant_size_differences)]
 
 extern crate nix;
 
